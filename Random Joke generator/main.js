b3920f21de9f7aa622ca9e0cc5463dca3e40e871
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-04-24 18:43:30 +0530

    joke: js

--- Random Joke generator/main.js
+++ Random Joke generator/main.js
@@ -1,2 +1,19 @@
-const h1 = document.querySelector('.h1');
-h1.textContent = 'hello there';
+const joke = document.querySelector('#joke');
+btn = document.querySelector('#btn');
+console.log(btn);
+const url =
+	'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
+
+const getJoke = () => {
+	joke.classList.remove('fade');
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			joke.textContent = data.joke;
+			joke.classList.add('fade');
+		});
+};
+btn.addEventListener('click', () => {
+	getJoke();
+});
+getJoke();
